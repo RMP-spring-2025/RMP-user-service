7f9ecb5e8a6754561a9a10b3ea98921ec405d95e
arslanefimov <dns230717rus@gmail.com>
2025-04-06 21:16:57 +0300

    add docker-compose for Data Base and add create_table.sql

---
+++ docker/postgres/init/create_tables.sql
@@ -0,0 +1,18 @@
+create table if not exists users(
+    id SERIAL PRIMARY KEY,
+    username varchar(50) UNIQUE NOT NULL,
+    age integer NOT NULL,
+    height DECIMAL(5,2),
+    weight DECIMAL(5, 2)
+    check ( age > 0 and height > 0 and weight > 0 )
+);
+
+create table if not exists user_products(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    product_id INTEGER NOT NULL,
+    mass_consumed INTEGER NOT NULL,
+    timestamp timestamp default now()
+    check ( mass_consumed > 0 )
+
+);
--- src/main/kotlin/infrastructure/persistance/DatabaseConfiguration.kt
+++ src/main/kotlin/infrastructure/persistance/DatabaseConfiguration.kt
@@ -4,9 +4,9 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 object DatabaseConfiguration {
-        private const val URL = "jdbc:postgresql://localhost:5432/your_database"
-        private const val USER = "user_name"
-        private const val PASSWORD = "your_password"
+        private const val URL = "jdbc:postgresql://localhost:5432/user_db"
+        private const val USER = "user_postgres"
+        private const val PASSWORD = "postgres"
 
         fun getConnection(): Connection {
             return DriverManager.getConnection(URL, USER, PASSWORD)
