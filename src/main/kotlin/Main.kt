d05a78eb585fd76a1cc517885992db0a03f72bef
Ternovkiy Ilya <50590135+lolmaxlevel@users.noreply.github.com>
2025-05-19 18:56:35 +0300

    fixes =)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -22,44 +22,44 @@ import org.healthapp.infrastructure.persistance.LiquibaseRunner
 import org.healthapp.util.KeyDBConnection
 
 fun main() {
-    "{\"productId\":45,\"time\":\"2025-04-24T20:36:44\",\"massConsumed\":432543.0,\"requestType\":\"add_product\",\"requestId\":\"e17c5fb2-14d8-4bda-a094-bfb3a8e10d29\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\"}".trimIndent()
-    "{\"requestType\":\"get_calories\",\"requestId\":\"9569e7cd-5627-4423-97d8-2c36679e4e32\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\",\"from\":[2025,4,24,20,0],\"to\":[2025,4,24,21,20,20]}".trimIndent()
-    "{\"requestType\":\"get_products\",\"requestId\":\"fec2298a-6369-4e98-850d-18098ed5957b\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\",\"from\":[2025,4,24,20,0],\"to\":[2025,4,24,21,20,20]}".trimIndent()
-    """
-        {
-          "requestType": "add_user",
-          "requestId": "${UUID.randomUUID()}",
-          "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
-          "username": "John Doe",
-          "age": 30,
-          "height": 175
-        }
-    """.trimIndent()
-    """
-        {
-            "requestType": "add_weight",
-            "requestId": "${UUID.randomUUID()}",
-            "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
-            "weight": ${Random.nextDouble()},
-            "time": "2025-04-24T20:36:44"
-        }
-    """.trimIndent()
-    val getWeightStatRequest = """
-        {   
-            "requestId": "${UUID.randomUUID()}",
-            "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
-            "requestType": "get_weight",
-            "from":[2025,4,24,20,0],
-            "to":[2025,4,24,21,20,20]
-        }
-    """.trimIndent()
-    val getUserStatRequest = """
-        {   
-            "requestId": "${UUID.randomUUID()}",
-            "userId": "6f0c05d2-1244-404e-88db-85222accbf6f",
-            "requestType": "get_user_stat"
-        }
-    """.trimIndent()
+    // "{\"productId\":45,\"time\":\"2025-04-24T20:36:44\",\"massConsumed\":432543.0,\"requestType\":\"add_product\",\"requestId\":\"e17c5fb2-14d8-4bda-a094-bfb3a8e10d29\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\"}".trimIndent()
+    // "{\"requestType\":\"get_calories\",\"requestId\":\"9569e7cd-5627-4423-97d8-2c36679e4e32\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\",\"from\":[2025,4,24,20,0],\"to\":[2025,4,24,21,20,20]}".trimIndent()
+    // "{\"requestType\":\"get_products\",\"requestId\":\"fec2298a-6369-4e98-850d-18098ed5957b\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\",\"from\":[2025,4,24,20,0],\"to\":[2025,4,24,21,20,20]}".trimIndent()
+    // """
+    //     {
+    //       "requestType": "add_user",
+    //       "requestId": "${UUID.randomUUID()}",
+    //       "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
+    //       "username": "John Doe",
+    //       "age": 30,
+    //       "height": 175
+    //     }
+    // """.trimIndent()
+    // """
+    //     {
+    //         "requestType": "add_weight",
+    //         "requestId": "${UUID.randomUUID()}",
+    //         "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
+    //         "weight": ${Random.nextDouble()},
+    //         "time": "2025-04-24T20:36:44"
+    //     }
+    // """.trimIndent()
+    // val getWeightStatRequest = """
+    //     {   
+    //         "requestId": "${UUID.randomUUID()}",
+    //         "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
+    //         "requestType": "get_weight",
+    //         "from":[2025,4,24,20,0],
+    //         "to":[2025,4,24,21,20,20]
+    //     }
+    // """.trimIndent()
+    // val getUserStatRequest = """
+    //     {   
+    //         "requestId": "${UUID.randomUUID()}",
+    //         "userId": "6f0c05d2-1244-404e-88db-85222accbf6f",
+    //         "requestType": "get_user_stat"
+    //     }
+    // """.trimIndent()
     LiquibaseRunner(System.getenv("rmp-user-service_DBChangelogFilePath")).runMigrations()
 
     val connection = KeyDBConnection()
