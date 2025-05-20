be00d1b7a25083577a998897c3dd40f56fff8ff7
ilestegor <99200163+ilestegor@users.noreply.github.com>
2025-05-20 21:40:08 +0300

    changed main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ package org.healthapp
 import ResponseAwaiter
 import app.service.AddProductConsumptionService
 import app.service.GetUserCaloriesService
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.healthapp.app.port.input.*
 import org.healthapp.app.port.output.UserDataRepository
@@ -21,48 +23,8 @@ import org.healthapp.infrastructure.handler.handlers.*
 import org.healthapp.infrastructure.handler.interfaces.RequestHandler
 import org.healthapp.infrastructure.persistance.LiquibaseRunner
 import org.healthapp.util.KeyDBConnection
-import java.util.UUID
-import kotlin.random.Random
 
-fun main() {
-    "{\"productId\":45,\"time\":\"2025-04-24T20:36:44\",\"massConsumed\":432543.0,\"requestType\":\"add_product\",\"requestId\":\"e17c5fb2-14d8-4bda-a094-bfb3a8e10d29\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\"}".trimIndent()
-    "{\"requestType\":\"get_calories\",\"requestId\":\"9569e7cd-5627-4423-97d8-2c36679e4e32\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\",\"from\":[2025,4,24,20,0],\"to\":[2025,4,24,21,20,20]}".trimIndent()
-    "{\"requestType\":\"get_products\",\"requestId\":\"fec2298a-6369-4e98-850d-18098ed5957b\",\"userId\":\"d39fb70b-2477-48e1-bc49-2bdbf742a12d\",\"from\":[2025,4,24,20,0],\"to\":[2025,4,24,21,20,20]}".trimIndent()
-    """
-        {
-          "requestType": "add_user",
-          "requestId": "${UUID.randomUUID()}",
-          "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
-          "username": "John Doe",
-          "age": 30,
-          "height": 175
-        }
-    """.trimIndent()
-    """
-        {
-            "requestType": "add_weight",
-            "requestId": "${UUID.randomUUID()}",
-            "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
-            "weight": ${Random.nextDouble()},
-            "time": "2025-04-24T20:36:44"
-        }
-    """.trimIndent()
-    val getWeightStatRequest = """
-        {   
-            "requestId": "${UUID.randomUUID()}",
-            "userId": "d39fb70b-2477-48e1-bc49-2bdbf742a12d",
-            "requestType": "get_weight",
-            "from":[2025,4,24,20,0],
-            "to":[2025,4,24,21,20,20]
-        }
-    """.trimIndent()
-    val getUserStatRequest = """
-        {   
-            "requestId": "${UUID.randomUUID()}",
-            "userId": "6f0c05d2-1244-404e-88db-85222accbf6f",
-            "requestType": "get_user_stat"
-        }
-    """.trimIndent()
+fun main() = runBlocking{
     LiquibaseRunner(System.getenv("rmp-user-service_DBChangelogFilePath") ?: "db/changelog/changelog-master.xml").runMigrations()
     val connection = KeyDBConnection()
     val responseAwaiter = ResponseAwaiter()
@@ -101,9 +63,16 @@ fun main() {
 
 
     val handlerRegistry = DefaultHandleRegistry(handlers)
-//    outputAdapter.sendRequest(getUserStatRequest)
 
     val input = RequestProcessor(KeyDBInputAdapter(connection), handlerRegistry, responseAwaiter)
     input.startListening()
-    Thread.sleep(Long.MAX_VALUE)
+    val shutDownSignal = CompletableDeferred<Unit>()
+    Runtime.getRuntime().addShutdownHook(Thread{
+        launch {
+            shutDownSignal.complete(Unit)
+            input.stop()
+            connection.shutDown()
+        }
+    })
+    shutDownSignal.await()
 }
